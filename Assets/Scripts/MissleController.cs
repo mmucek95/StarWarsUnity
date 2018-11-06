575ed23c0123d258636ff2c16597cff67cc1c9a6
unknown <m_mucek@wp.pl>
2018-11-06 22:29:15 +0100

    Ship shooting missles

---
+++ Assets/Scripts/MissleController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MissleController : MonoBehaviour {
+
+    public float missleSpeed = 1.0f, missleAcceleration = 1.0f;
+    public RaycastHit hit;
+    bool left;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Translate(this.transform.up * missleSpeed * Time.deltaTime, Space.World);
+        missleSpeed += missleAcceleration * Time.deltaTime;
+        Debug.DrawRay(this.gameObject.transform.position, Vector3.up);
+
+        if(Physics.Raycast(this.gameObject.transform.position, this.gameObject.transform.up, out hit, 0.5f))
+        {
+            Explosion();
+        }
+	}
+
+    void Explosion()
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/SpaceShipController.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpaceShipController : MonoBehaviour {
+
+    public MissleController missle;
+    private float axisX = 0f, axisY = 0f;
+    public float maxForce = 0f;
+    public ParticleSystem upperLeft, upperRight, upperMiddleLeft, upperMiddleRight,
+        downMiddleLeft, downMiddleRight, downLeft, downRight;
+	// Use this for initialization
+	void Start () {
+        upperLeft.enableEmission = false;
+        upperRight.enableEmission = false;
+        upperMiddleLeft.enableEmission = false;
+        upperMiddleRight.enableEmission = false;
+        downMiddleLeft.enableEmission = false;
+        downMiddleRight.enableEmission = false;
+        downLeft.enableEmission = false;
+        downRight.enableEmission = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        axisX = Input.GetAxisRaw("Horizontal") * Time.deltaTime;
+        axisY = Input.GetAxisRaw("Vertical") * Time.deltaTime;
+
+        if(axisX == 0 || axisY == 0)
+        {
+            this.GetComponent<Rigidbody>().AddForce(new Vector3(axisX, axisY, 0) * maxForce);
+        }
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            upperMiddleRight.enableEmission = true;
+            downMiddleRight.enableEmission = true;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            upperMiddleRight.enableEmission = false;
+            downMiddleRight.enableEmission = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            upperMiddleLeft.enableEmission = true;
+            downMiddleLeft.enableEmission = true;
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            upperMiddleLeft.enableEmission = false;
+            downMiddleLeft.enableEmission = false;
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            downLeft.enableEmission = true;
+            downRight.enableEmission = true;
+        }
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            downLeft.enableEmission = false;
+            downRight.enableEmission = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            upperLeft.enableEmission = true;
+            upperRight.enableEmission = true;
+        }
+        if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            upperLeft.enableEmission = false;
+            upperRight.enableEmission = false;
+        }
+
+        if(Input.GetKeyDown(KeyCode.T))
+        {
+            Instantiate(missle, this.transform.position + new Vector3(0,50, 0), Quaternion.Euler(0, 0, 0));
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Instantiate(missle, this.transform.position + new Vector3(0, -50, 0), Quaternion.Euler(180, 90, 0));
+        }
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            Instantiate(missle, this.transform.position + new Vector3(50, 0, 0), Quaternion.Euler(90, 90, 0));
+        }
+
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            Instantiate(missle, this.transform.position + new Vector3(-50, 0, 0), Quaternion.Euler(270, 90, 0));
+        }
+    }
+}
